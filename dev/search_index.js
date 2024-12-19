746591cafbd00cc71239c16eb9dd7e82746b07e2
Documenter.jl <documenter@juliadocs.github.io>
2024-12-19 13:14:02 +0000

    build based on b1bfe24

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SatisFactoring","category":"page"},{"location":"#SatisFactoring","page":"Home","title":"SatisFactoring","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SatisFactoring.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SatisFactoring]","category":"page"}]
+}
